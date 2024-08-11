676c4b302d5462bdc4feb29ff0cfb4ef443577cb
Joseph Ruff <joegruff16@gmail.com>
2024-08-11 17:12:28 -0400

    Reran build in hope that Render can find the index file now.

--- server/server.js
+++ server/server.js
@@ -6,11 +6,11 @@
 // 3. Create an Apollo Provider so that requests can communicate with an Apollo Server.
 
 // 4. Deploy your application to Render with a MongoDB database using MongoDB Atlas.
-
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+// Having issues.
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,12 +19,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.use(routes);
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
