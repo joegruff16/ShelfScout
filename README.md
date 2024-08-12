698b95ac26901e38ff64e77e9987c2a42e6a60bc
Joseph Ruff <joegruff16@gmail.com>
2024-08-11 20:09:02 -0400

    Updated README to include the livelink and repo. Updated the server.js file to include packages for ApolloServer, ExpressMiddleWare and the piping to set up starting the apolloserver.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ShelfScout
+
+## Live Link
+
+<https://shelfscout-deploy.onrender.com/>
+
+## GitHub Repo
+
+<https://github.com/joegruff16>
--- server/server.js
+++ server/server.js
@@ -12,23 +12,44 @@
 // With apollo-server-express we need to use the expressMiddleware function
 
 const express = require("express");
+// Import ApolloServer and ExpressMiddleware to convert to GraphQL
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleWare } = require("@apollo/server/express4");
+
 const path = require("path");
+// Will need to import typeDefs and Resolvers here when created
 const db = require("./config/connection");
 const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
-
-app.use(routes);
-
-db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+const server = new ApolloServer({
+  // typeDefs,
+  // resolvers, This will house the typeDefs and resolvers when created
 });
+
+const startApolloServer = async () => {
+  await server.start();
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  app.use("/graphql", expressMiddleWare(server));
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  }
+
+  app.use(routes); // This will be going away when we have established our Resolvers and TypeDefs
+
+  // Updated to add another console.log to show where the graphql server is running
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 API server is listening on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+// Invoke the apolloServer
+startApolloServer();
